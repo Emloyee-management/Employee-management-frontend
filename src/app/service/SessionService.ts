d3dd2c70160c3025b65ac300d4c5fc5207fb45fe
Zishan Liu <zisaanlau@gmail.com>
2020-08-02 17:42:17 -0400

    add axios

---
+++ src/app/service/SessionService.ts
@@ -0,0 +1,7 @@
+//   getLoginInfo = (username: String, password: String) => {
+//     axios.get(`/login?username=${username}&password=${password}`).then((info) => {
+//       // Router.navigate('');
+//     }).catch((e) => {
+//       console.info(e);
+//     })
+//   }
