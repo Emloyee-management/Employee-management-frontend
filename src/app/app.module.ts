1daf7c07e6e8d8e694de12bb3245ee93dbf32ef9
Zishan Liu <zisaanlau@gmail.com>
2020-08-02 22:14:52 -0400

    login frontend finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { StartingPageComponent } from './component/starting-page/starting-page.component';
 import { ErrorPageComponent } from './component/error-page/error-page.component';
 import { UserPageComponent } from './component/user-page/user-page.component';
-
+import { LoginPageComponent } from './component/login-page/login-page.component';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [AppComponent, StartingPageComponent, ErrorPageComponent, UserPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    ErrorPageComponent,
+    UserPageComponent,
+    LoginPageComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
